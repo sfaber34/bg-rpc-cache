3e47d55e2410370e7c4ee3c7b6aa7c2f05bd5540
Ubuntu <ubuntu@ip-172-31-89-145.ec2.internal>
2025-02-03 22:30:39 +0000

    Made it check block number every second and add it to a map. Also made it serve cache over http

---
+++ cache.js
@@ -0,0 +1,48 @@
+import http from "http";
+import { createPublicClient, http as httpTransport } from "viem";
+import { mainnet } from "viem/chains";
+
+import { fallbackUrl, cachePort } from "./config.js";
+
+const cacheMap = new Map();
+
+function getMapContents() {
+  const entries = {};
+  for (const [key, value] of cacheMap.entries()) {
+    entries[key] = typeof value === 'bigint' ? value.toString() : value;
+  }
+  return {
+    size: cacheMap.size,
+    entries
+  };
+}
+
+// Create HTTP server to serve map contents
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(getMapContents(), null, 2));
+});
+
+server.listen(cachePort, () => {
+  console.log("----------------------------------------------------------------------------------------------------------------");
+  console.log("----------------------------------------------------------------------------------------------------------------");
+  console.log(`Cache server running at http://localhost:${cachePort}`);
+});
+
+export const fallbackClient = createPublicClient({
+  name: "RPC",
+  chain: mainnet,
+  transport: httpTransport(fallbackUrl),
+});
+
+async function setCache() {
+  const blockNumber = await fallbackClient.getBlockNumber();
+  const currentCachedBlock = cacheMap.get('blockNumber') || 0;
+  
+  if (blockNumber > currentCachedBlock) {
+    cacheMap.set('blockNumber', blockNumber);
+    console.log("Scaffold RPC is on block\t" + blockNumber);
+  }
+}
+
+setInterval(setCache, 1000);
---
+++ config.js
@@ -0,0 +1,3 @@
+export const fallbackUrl = "https://office.buidlguidl.com:48544";
+export const cachePort = 3002;
+export const fallbackRequestTimeout = 15000;
